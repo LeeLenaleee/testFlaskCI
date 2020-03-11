96fadcb8cdf0d8f47922c87acc24ed457ae1f9e0
Jacco van den Berg <jaccoberg2281@gmail.com>
2020-03-11 22:12:38 +0100

    run je nu nog steeds op een runner of runde je op de runner op me laptop

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, World! Pep heeft corona keer twee met tico"
+	
+@app.route('/test')
+def indexx():
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True)
